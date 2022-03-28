a8381bf8c2850c2bcc2c4c8c2954f028f122555b
Oleksandr Filonenko <filonenko.oleksa@gmail.com>
2022-03-28 17:52:13 +0300

    Added the starting page

--- ktor-app/src/main/kotlin/edu/xfoleks/plugins/Routing.kt
+++ ktor-app/src/main/kotlin/edu/xfoleks/plugins/Routing.kt
@@ -3,10 +3,14 @@ package edu.xfoleks.plugins
 import edu.xfoleks.routes.randomImage
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        get("/") {
+            call.respondText { "The start page" }
+        }
         randomImage()
         // Static plugin. Try to access `/static/index.html`
         static {
